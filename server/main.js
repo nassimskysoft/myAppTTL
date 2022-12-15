1aecff241ad0f7b5cdd159d0a2172fe17248f4a8
nassimskysoft <nassimetude@gmail.com>
2022-12-15 01:17:00 +0100

    fix mongodb bug2

--- server/main.js
+++ server/main.js
@@ -4,7 +4,7 @@ import { ExportCollection } from '../imports/api/ExportCollection';
 Meteor.startup(() => {
   //init mongoDB si la collection n'existe pas pour créer la structure
   if (ExportCollection.find().count() === 0) {
-    let id = ExportCollection.insert({ text: 'taskText' });
+    let id = ExportCollection.insert({ value: 0 });
     ExportCollection.remove({ _id: id });
   }
 });
